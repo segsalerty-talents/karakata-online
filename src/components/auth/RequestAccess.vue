afc43b4f27c5cda9696e369172c32981e8276581
Olakunle Boye <kunlexzy@gmail.com>
2022-02-19 00:00:58 +0100

    create request access component
    Signed-off-by: Olakunle Boye <kunlexzy@gmail.com>

---
+++ src/components/auth/RequestAccess.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container">
+    <c-stack spacing="3">
+      <c-heading as="h1" size="xl">
+        Request for access
+      </c-heading>
+      <c-heading as="h5" size="sm">
+        Tell us a little about your business, let us process your access on Karakata.
+      </c-heading>
+    </c-stack>
+  </div>
+</template>
--- src/views/auth/Register.vue
+++ src/views/auth/Register.vue
@@ -1,3 +1,57 @@
 <template>
-<div>This register</div>
+<div class="register">
+  <header class="container">
+    <c-image :src="require('@/assets/img/Logo1.svg')" w="80px" />
+  </header>
+  <div class="">
+    <request-access />
+  </div>
+</div>
 </template>
+
+<script>
+import RequestAccess from '@/components/auth/RequestAccess'
+
+export default {
+  components: {
+    RequestAccess
+  }
+}
+</script>
+
+<style scoped lang="css">
+.register {
+  width: 100%;
+  height: 100%;
+}
+.register .container {
+  width: 100%;
+  margin-right: auto;
+  margin-left: auto;
+  padding: 15px;
+}
+
+@media (min-width: 576px) {
+  .register .container {
+    max-width: 540px;
+  }
+}
+
+@media (min-width: 768px) {
+  .register .container {
+    max-width: 720px;
+  }
+}
+
+@media (min-width: 992px) {
+  .register .container {
+    max-width: 960px;
+  }
+}
+
+@media (min-width: 1200px) {
+  .register .container {
+    max-width: 1140px;
+  }
+}
+</style>
