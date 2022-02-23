c12c3205dcb1828f725215b00a81bea0f75b8155
Olakunle Boye <kunlexzy@gmail.com>
2022-02-23 07:47:52 +0100

    finish up registration page
    Signed-off-by: Olakunle Boye <kunlexzy@gmail.com>

--- src/components/auth/RequestAccess.vue
+++ src/components/auth/RequestAccess.vue
@@ -1,57 +1,60 @@
 <template>
-  <div class="container">
-    <c-stack spacing="3">
-      <c-heading as="h1" size="xl" color="#393939">
-        Request for access
-      </c-heading>
-      <c-heading as="h5" size="sm" color="#666666">
-        Tell us a little about your business, let us process your access on Karakata.
-      </c-heading>
-    </c-stack>
-    <c-flex :direction="{ base: 'column', sm: 'row' }" mt="8">
-      <c-box width="100%" mr="1" mb="1">
-        <c-flex mr="-7.5" ml="-7.5" :direction="{ base: 'column', sm: 'row' }">
-          <c-form-control mr="7.5" ml="7.5" pb="5" is-required>
-            <c-form-label for="fname" color="#999999">Business name</c-form-label>
-            <c-input id="fname" border-radius="7" border-color="#E27253" box-shadow="0 0 0 0 #E27253" bg="#FFFAF9" />
-          </c-form-control>
-          <c-form-control mr="7.5" ml="7.5" pb="5" is-required>
-            <c-form-label for="fname" color="#999999">Business telephone</c-form-label>
-            <c-input id="fname" border-radius="7" border-color="#E27253" box-shadow="0 0 0 0 #E27253" bg="#FFFAF9" />
-          </c-form-control>
-        </c-flex>
-        <c-flex mr="-7.5" ml="-7.5" :direction="{ base: 'column', sm: 'row' }">
-          <c-form-control mr="7.5" ml="7.5" pb="5" is-required>
-            <c-form-label for="fname" color="#999999">Business name</c-form-label>
-            <c-input id="fname" border-radius="7" border-color="#E27253" box-shadow="0 0 0 0 #E27253" bg="#FFFAF9" />
-          </c-form-control>
-          <c-form-control mr="7.5" ml="7.5" pb="5" is-required>
-            <c-form-label for="fname" color="#999999">Business telephone</c-form-label>
-            <c-input id="fname" border-radius="7" border-color="#E27253" box-shadow="0 0 0 0 #E27253" bg="#FFFAF9" />
-          </c-form-control>
-        </c-flex>
-        <c-flex mr="-7.5" ml="-7.5" :direction="{ base: 'column', sm: 'row' }">
-          <c-form-control mr="7.5" ml="7.5" pb="5" is-required>
-            <c-form-label for="fname" color="#999999">Business name</c-form-label>
-            <c-input id="fname" border-radius="7" border-color="#E27253" box-shadow="0 0 0 0 #E27253" bg="#FFFAF9" />
-          </c-form-control>
-          <c-form-control mr="7.5" ml="7.5" pb="5" is-required>
-            <c-form-label for="fname" color="#999999">Business telephone</c-form-label>
-            <c-input id="fname" border-radius="7" border-color="#E27253" box-shadow="0 0 0 0 #E27253" bg="#FFFAF9" />
-          </c-form-control>
-        </c-flex>
-        <c-flex mr="-7.5" ml="-7.5" :direction="{ base: 'column', sm: 'row' }">
-          <c-form-control mr="7.5" ml="7.5" pb="5" is-required>
-            <c-form-label for="fname" color="#999999">Business name</c-form-label>
-            <c-input id="fname" border-radius="7" border-color="#E27253" box-shadow="0 0 0 0 #E27253" bg="#FFFAF9" />
-          </c-form-control>
-          <c-form-control mr="7.5" ml="7.5" pb="5" is-required>
-            <c-form-label for="fname" color="#999999">Business telephone</c-form-label>
-            <c-input id="fname" border-radius="7" border-color="#E27253" box-shadow="0 0 0 0 #E27253" bg="#FFFAF9" />
-          </c-form-control>
-        </c-flex>
-      </c-box>
-      <c-box width="100%" bg="orange.50" h="2"></c-box>
+  <c-box width="100%" mr="1" mb="1">
+    <c-flex w="100%" mr="-7.5" ml="-7.5" :direction="{ base: 'column', sm: 'row' }">
+      <c-form-control w="100%" mr="7.5" ml="7.5" pb="5" is-required>
+        <c-form-label for="fname" color="#999999">Business name</c-form-label>
+        <c-input id="fname" border-radius="7" border-color="#E27253" box-shadow="0 0 0 0 #E27253" bg="#FFFAF9" />
+      </c-form-control>
+      <c-form-control w="100%" mr="7.5" ml="7.5" pb="5" is-required>
+        <c-form-label for="fname" color="#999999">Business telephone</c-form-label>
+        <c-input-group>
+          <c-input-left-addon border-color="#E27253" box-shadow="0 0 0 0 #E27253" bg="#FFFAF9">+234</c-input-left-addon>
+          <c-input type="tel" roundedLeft="0" placeholder="phone number" border-radius="7" border-color="#E27253" box-shadow="0 0 0 0 #E27253" bg="#FFFAF9" />
+        </c-input-group>
+      </c-form-control>
     </c-flex>
-  </div>
+    <c-flex w="100%" mr="-7.5" ml="-7.5" :direction="{ base: 'column', sm: 'row' }">
+      <c-form-control w="100%" mr="7.5" ml="7.5" pb="5" is-required>
+        <c-form-label for="fname" color="#999999">Business address</c-form-label>
+        <c-input id="fname" border-radius="7" border-color="#E27253" box-shadow="0 0 0 0 #E27253" bg="#FFFAF9" />
+      </c-form-control>
+      <c-form-control w="100%" mr="7.5" ml="7.5" pb="5" is-required>
+        <c-form-label for="fname" color="#999999">Business state</c-form-label>
+        <c-input id="fname" border-radius="7" border-color="#E27253" box-shadow="0 0 0 0 #E27253" bg="#FFFAF9" />
+      </c-form-control>
+    </c-flex>
+    <c-flex w="100%" mr="-7.5" ml="-7.5" :direction="{ base: 'column', sm: 'row' }">
+      <c-form-control w="100%" mr="7.5" ml="7.5" pb="5" is-required>
+        <c-form-label for="fname" color="#999999">Business country</c-form-label>
+        <c-input id="fname" border-radius="7" border-color="#E27253" box-shadow="0 0 0 0 #E27253" bg="#FFFAF9" />
+      </c-form-control>
+      <c-form-control w="100%" mr="7.5" ml="7.5" pb="5" is-required>
+        <c-form-label for="fname" color="#999999">Business month of establishment</c-form-label>
+        <c-select placeholder="Select Month" color="#999999" border-radius="7" border-color="#E27253" box-shadow="0 0 0 0 #E27253" bg="#FFFAF9">
+          <option value="grilled">01</option>
+          <option value="pub-style">02</option>
+          <option value="jucy-lucy">03</option>
+        </c-select>
+      </c-form-control>
+    </c-flex>
+    <c-flex w="100%" mr="-7.5" ml="-7.5" :direction="{ base: 'column', sm: 'row' }">
+      <c-form-control w="100%" mr="7.5" ml="7.5" pb="5" is-required>
+        <c-form-label for="fname" color="#999999">Business year of establishment</c-form-label>
+        <c-select placeholder="Select Year" color="#999999" border-radius="7" border-color="#E27253" box-shadow="0 0 0 0 #E27253" bg="#FFFAF9">
+          <option value="grilled">2019</option>
+          <option value="pub-style">2021</option>
+          <option value="jucy-lucy">2022</option>
+        </c-select>
+      </c-form-control>
+      <c-form-control w="100%" mr="7.5" ml="7.5" pb="5" is-required>
+        <c-form-label for="fname" color="#999999">Business kind</c-form-label>
+        <c-input id="fname" border-radius="7" border-color="#E27253" box-shadow="0 0 0 0 #E27253" bg="#FFFAF9" />
+      </c-form-control>
+    </c-flex>
+    <c-flex justify="center" mt="8">
+      <c-button @click="$emit('next-stage')" right-icon="arrow-forward" size="md" color="#FFFFFF" border-radius="18px" bg="#E27253;" border="1px" border-color="#E27253">
+        Next
+      </c-button>
+    </c-flex>
+  </c-box>
 </template>
--- src/views/auth/Register.vue
+++ src/views/auth/Register.vue
@@ -3,18 +3,57 @@
   <header class="container">
     <c-image :src="require('@/assets/img/Logo1.svg')" w="80px" />
   </header>
-  <div class="">
-    <request-access />
+  <div class="container">
+    <c-stack spacing="3" v-if="step === 1">
+      <c-heading as="h1" size="xl" color="#393939">
+        Request for access
+      </c-heading>
+      <c-heading as="h5" size="sm" color="#666666">
+        Tell us a little about your business, let us process your access on Karakata.
+      </c-heading>
+    </c-stack>
+    <c-stack spacing="3" v-if="step === 2">
+      <c-heading as="h1" size="xl" color="#393939">
+        Set up
+      </c-heading>
+      <c-heading as="h5" size="sm" color="#666666">
+        Let’s quickly set you your business on Karakata.
+      </c-heading>
+    </c-stack>
+    <c-flex :direction="{ base: 'column', sm: 'row' }" mt="8">
+      <c-box width="100%">
+        <request-access v-if="step === 1" @next-stage="nextStage"/>
+        <set-up v-if="step === 2" @prev-stage="prevStage" />
+      </c-box>
+      <c-box :display="{ base: 'none', sm: 'block' }" width="100%" mt="-40">
+        <c-image :src="require('@/assets/img/Mobilelogin-amico1.svg')" />
+      </c-box>
+    </c-flex>
   </div>
 </div>
 </template>
 
 <script>
 import RequestAccess from '@/components/auth/RequestAccess'
+import SetUp from '@/components/auth/SetUp'
 
 export default {
   components: {
-    RequestAccess
+    RequestAccess,
+    SetUp
+  },
+  data () {
+    return {
+      step: 1
+    }
+  },
+  methods: {
+    nextStage () {
+      this.step++
+    },
+    prevStage () {
+      this.step--
+    }
   }
 }
 </script>
@@ -23,6 +62,9 @@ export default {
 .register {
   width: 100%;
   height: 100%;
+}
+.register input {
+  width: 100%;
 }
 .register .container {
   width: 100%;
