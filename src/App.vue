eb8f018f403d5251151d0f98bb9caf4a4d5ac1ab
blade-01 <animashauntaofiq@gmail.com>
2022-02-27 03:15:30 +0100

    Plugin: Add vuelidate (form validation plugin)

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
-  <div id="app">
+  <div id="app" font-family="'Lato', sans-serif">
     <router-view />
   </div>
 </template>
 
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap');
-body {
+body,
+html,
+#app,
+* {
   font-family: 'Lato', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
