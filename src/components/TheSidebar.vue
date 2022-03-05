8e2c2640db292e7447b7bfbf634ea35314b036a3
Olakunle Boye <kunlexzy@gmail.com>
2022-03-06 00:17:40 +0100

    Style: added dashboard icon

--- src/components/TheSidebar.vue
+++ src/components/TheSidebar.vue
@@ -10,7 +10,32 @@
     mt="30px"
     w="100%"
     justify="center">
-    <c-image :src="require('@/assets/img/Logo2.svg')" w="100px" />
+    <c-image :src="require('@/assets/img/Logo2.svg')" w="120px" />
   </c-flex>
+  <c-box pr="15px" pl="15px" mt="80px">
+  <c-list styleType="none">
+    <c-list-item bg="#FFBC9F" p="15px" borderRadius="10px">
+      <router-link
+        to="/dashboad"
+      >
+      <c-image
+        :src="require('@/assets/img/dashboard-icon.svg')"
+        w="25px"
+        mr="15px"
+        display="inline-block"
+      />
+      <c-box
+        as="span"
+        fontSize="25px"
+        color="#fff"
+        verticalAlign="middle"
+        fontWeight="bold"
+      >
+        Dashboard
+      </c-box>
+      </router-link>
+    </c-list-item>
+  </c-list>
+  </c-box>
   </c-box>
 </template>
