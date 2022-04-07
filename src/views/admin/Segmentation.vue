e8f647127d37bd9cfab25d727d42c039ddc2cc92
Olakunle Boye <kunlexzy@gmail.com>
2022-04-08 00:31:25 +0100

    implementing segmentation ui

---
+++ src/views/admin/Segmentation.vue
@@ -0,0 +1,29 @@
+<template>
+  <c-box
+    w="100%"
+    pl="4"
+    pr="4">
+    <c-flex>
+      <c-box width="100%">
+        <c-box
+          pb="4"
+          border-bottom="1px solid rgba(130, 134, 154, 0.51)">
+          <c-heading as="h3" size="md">Phone Number</c-heading>
+        </c-box>
+      </c-box>
+      <c-box width="100%">
+      <c-box
+        pb="4"
+        border-bottom="1px solid rgba(130, 134, 154, 0.51)">
+        <c-heading
+          as="h3"
+          size="md"
+          text-align="left"
+        >
+        Message
+        </c-heading>
+      </c-box>
+      </c-box>
+    </c-flex>
+  </c-box>
+</template>
