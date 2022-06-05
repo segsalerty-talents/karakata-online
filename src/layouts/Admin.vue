351c7919a32d8b4a8db7b37d64dee3dcfd97eabb
blade-01 <animashauntaofiq@gmail.com>
2022-06-05 12:41:19 +0100

    Update: Commented out some stuffs

--- src/layouts/Admin.vue
+++ src/layouts/Admin.vue
@@ -1,21 +1,20 @@
 <template>
   <div>
     <!-- <sidebar /> -->
-    <div class="relative md:ml-64 bg-blueGray-100">
-      <!-- <admin-navbar /> -->
-      <!-- <header-stats /> -->
-      <div class="px-4 md:px-10 mx-auto w-full -m-24">
-        <router-view />
-        <!-- <footer-admin /> -->
-      </div>
-    </div>
+    <!-- <div class="relative md:ml-64 bg-blueGray-100"> -->
+    <!-- <admin-navbar /> -->
+    <!-- <header-stats /> -->
+    <!-- <div class="px-4 md:px-10 mx-auto w-full -m-24"> -->
+    <router-view />
+    <!-- <footer-admin /> -->
+    <!-- </div> -->
+    <!-- </div> -->
   </div>
 </template>
 
 <script>
 export default {
   name: 'admin-layout',
-  components: {
-  }
+  components: {}
 }
 </script>
