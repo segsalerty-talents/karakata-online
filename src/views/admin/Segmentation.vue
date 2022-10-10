9793277da2e535b8f7ed72442f4ce8a1b6afa550
Olakunle Boye <kunlexzy@gmail.com>
2022-10-10 10:06:51 +0100

    updating segmentation ui

--- src/views/admin/Segmentation.vue
+++ src/views/admin/Segmentation.vue
@@ -11,25 +11,31 @@
         <c-button
           color="#393939"
           bg="transparent"
-          border=" 1px solid #9E9E9E"
+          border="1px solid #9E9E9E"
           borderRadius="20"
-          fontWeight="400">
+          fontWeight="400"
+          :_hover="{opacity: 0.8}">
           <i style="padding-right: 6px;"><SettingIcon /></i>
           Segments
         </c-button>
-        <c-button variant-color="blue" variant="outline">
-          Button
+        <c-button
+          bg="#E27253"
+          color="#FFF"
+          :_hover="{opacity: 0.8}"
+          borderRadius="8">
+          All Segments
         </c-button>
       </c-button-group>
     </c-box>
     <c-flex :direction="{base: 'column', md: 'row'}">
-      <!-- <c-box width="100%">
+      <c-box width="100%">
         <c-box
-          pb="4"
-          border-bottom="1px solid rgba(130, 134, 154, 0.51)">
-          <c-heading as="h3" size="md">Phone Number</c-heading>
-        </c-box>
-        <c-box mt="5">
+          mt="5"
+          border="1px solid #9E9E9E"
+          p="5"
+          borderRadius="5"
+          height="500px"
+          overflow="auto">
           <c-checkbox-group variant-color="blue" :default-value="['naruto', 'kakashi']">
             <c-checkbox
               :is-checked="allChecked"
@@ -37,32 +43,28 @@
               @change="(val, $e) => { checkedItems = [$e.target.checked, $e.target.checked] }"
             ><c-heading as="h3" size="sm">Select all</c-heading></c-checkbox>
               <c-checkbox
-                v-for="(d,i) in 4"
+                v-for="(d,i) in 40"
                 :key="i"
                 :is-checked="checkedItems[i]"
                 @change="(val, $e) => { checkedItems = [$e.target.checked, checkedItems[i]] }"
               >+234 9047738232</c-checkbox>
           </c-checkbox-group>
         </c-box>
-      </c-box> -->
-      <!-- <c-box width="100%" :mt="{base: '20px', md: 0}">
-      <c-box
-        pb="4"
-        border-bottom="1px solid rgba(130, 134, 154, 0.51)">
-        <c-heading
-          as="h3"
-          size="md"
-          text-align="left"
-        >
-        Message
-        </c-heading>
       </c-box>
-      <c-box mt="5">
-          <c-flex :max-width="{base: '100%', md: '80%', xl: '50%'}" justify="space-between">
+      <c-box
+        width="100%"
+        :mt="{base: '20px', md: 0}">
+      <c-box mt="5"
+        :ml="{base: 0 , md: 5}"
+        :width="{base: '100%', md: '50%'}"
+        border="1px solid #9E9E9E"
+        p="5"
+        borderRadius="5">
+          <c-flex :max-width="{base: '100%', md: '80%', xl: '100%'}" justify="space-between">
             <c-heading as="h4" size="sm">Input Message</c-heading>
             <c-link>view template</c-link>
           </c-flex>
-          <c-box :max-width="{base: '100%', md: '80%', xl: '50%'}" mt="5px">
+          <c-box :max-width="{base: '100%', md: '80%', xl: '100%'}" mt="5px">
            <c-textarea is-invalid placeholder="Here is a sample placeholder" />
            <c-button
               size="lg"
@@ -79,7 +81,7 @@
           </c-button>
           </c-box>
         </c-box>
-      </c-box> -->
+      </c-box>
     </c-flex>
   </c-box>
 </template>
