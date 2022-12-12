63ae889710e3eeb164fda1c61e997b654dc447b4
Olakunle Boye <kunlexzy@gmail.com>
2022-12-12 19:25:58 +0100

    Up segmentation (#35)
    * optimized docker
    
    * removed template
    
    * Added: sidebar link components
    
    * moved svg into components
    
    * set active css property for svg
    
    * fixed link mouseover style property
    
    * fixed icon components
    
    * Added: dashboard svg component
    
    * Added: segment button
    
    Signed-off-by: Olakunle Boye <kunlexzy@gmail.com>
    
    * updating segmentation ui
    
    * iimplenetd enter message box
    
    * Added: segmentation modal
    
    Signed-off-by: Olakunle Boye <kunlexzy@gmail.com>
    
    * update modal button
    
    Signed-off-by: Olakunle Boye <kunlexzy@gmail.com>
    
    Signed-off-by: Olakunle Boye <kunlexzy@gmail.com>

--- src/views/admin/Segmentation.vue
+++ src/views/admin/Segmentation.vue
@@ -3,14 +3,42 @@
     w="100%"
     pl="4"
     pr="4">
+    <c-stack>
+      <c-text fontSize="4xl">Segmentation</c-text>
+    </c-stack>
+    <c-box mt="8">
+      <c-button-group spacing="4">
+        <c-button
+          color="#393939"
+          bg="transparent"
+          border="1px solid #9E9E9E"
+          borderRadius="20"
+          fontWeight="400"
+          :_hover="{opacity: 0.8}"
+          @click="open"
+        >
+          <i style="padding-right: 6px;"><SettingIcon /></i>
+          Segments
+        </c-button>
+        <c-button
+          bg="#E27253"
+          color="#FFF"
+          :_hover="{opacity: 0.8}"
+          borderRadius="8">
+          All Segments
+        </c-button>
+      </c-button-group>
+    </c-box>
     <c-flex :direction="{base: 'column', md: 'row'}">
-      <c-box width="100%">
+      <c-box :width="{base: '100%', md: '40%'}">
         <c-box
-          pb="4"
-          border-bottom="1px solid rgba(130, 134, 154, 0.51)">
-          <c-heading as="h3" size="md">Phone Number</c-heading>
-        </c-box>
-        <c-box mt="5">
+          mt="5"
+          border="1px solid #9E9E9E"
+          p="5"
+          borderRadius="5"
+          height="500px"
+          overflow="auto"
+          :width="{base: '100%', md: '50%'}">
           <c-checkbox-group variant-color="blue" :default-value="['naruto', 'kakashi']">
             <c-checkbox
               :is-checked="allChecked"
@@ -18,7 +46,7 @@
               @change="(val, $e) => { checkedItems = [$e.target.checked, $e.target.checked] }"
             ><c-heading as="h3" size="sm">Select all</c-heading></c-checkbox>
               <c-checkbox
-                v-for="(d,i) in 4"
+                v-for="(d,i) in 40"
                 :key="i"
                 :is-checked="checkedItems[i]"
                 @change="(val, $e) => { checkedItems = [$e.target.checked, checkedItems[i]] }"
@@ -26,50 +54,160 @@
           </c-checkbox-group>
         </c-box>
       </c-box>
-      <c-box width="100%" :mt="{base: '20px', md: 0}">
       <c-box
-        pb="4"
-        border-bottom="1px solid rgba(130, 134, 154, 0.51)">
-        <c-heading
-          as="h3"
-          size="md"
-          text-align="left"
-        >
-        Message
-        </c-heading>
-      </c-box>
-      <c-box mt="5">
-          <c-flex :max-width="{base: '100%', md: '80%', xl: '50%'}" justify="space-between">
-            <c-heading as="h4" size="sm">Input Message</c-heading>
-            <c-link>view template</c-link>
+        :width="{base: '100%', md: '50%'}"
+        :mt="{base: '20px', md: 0}">
+      <c-box mt="5"
+        :ml="{base: 0 , md: 5}"
+        :width="{base: '100%', md: '70%'}"
+        border="1px solid #9E9E9E"
+        p="5"
+        borderRadius="5">
+          <c-flex :max-width="{base: '100%', md: '80%', xl: '100%'}" justify="space-between">
+            <c-heading as="h4" size="sm" fontWeight="400">Message</c-heading>
           </c-flex>
-          <c-box :max-width="{base: '100%', md: '80%', xl: '50%'}" mt="5px">
-           <c-textarea is-invalid placeholder="Here is a sample placeholder" />
-           <c-button
-              size="lg"
-              color="#FFFFFF"
-              border-radius="16px"
-              bg="#E27253;"
-              border="1px"
-              border-color="#E27253"
-              mt="4"
-              width="100%"
-              :hover="{bg: '#E27253', opacity: .8}"
-            >
-            Send message
-          </c-button>
+          <c-box :max-width="{base: '100%', md: '80%', xl: '100%'}" mt="5px">
+           <c-textarea placeholder="Enter message" />
+           <c-flex>
+            <c-button
+                size="md"
+                color="#FFFFFF"
+                border-radius="16px"
+                bg="#E27253;"
+                border="1px"
+                border-color="#E27253"
+                mt="4"
+                width="80%"
+                :_hover="{bg: '#E27253', opacity: .8}"
+                mr="2"
+              >
+                Send
+              </c-button>
+              <c-button
+                size="md"
+                color="#FFFFFF"
+                border-radius="16px"
+                bg="#E27253;"
+                border="1px"
+                border-color="#E27253"
+                mt="4"
+                width="20%"
+                :_hover="{bg: '#E27253', opacity: .8}"
+              >
+                <c-icon name="close" size="12px" />
+              </c-button>
+            </c-flex>
           </c-box>
         </c-box>
       </c-box>
     </c-flex>
+    <!-- Modal -->
+    <c-modal
+      :is-open="isOpen"
+      :on-close="close"
+      size="xs"
+    >
+      <c-modal-content ref="content">
+        <c-modal-header>Date</c-modal-header>
+        <c-modal-close-button />
+        <c-modal-body>
+          <c-flex justify="space-between">
+            <c-box width="50%" mr="2">
+            <c-button
+              bg="transparent"
+              borderRadius="15"
+              border="1px solid #9E9E9E"
+              size="md"
+              fontWeight="400"
+              fontSize="14px"
+              width="100%"
+              :_hover="{bg: 'transparent'}"
+            >
+              <i><CalenderIcon /></i>
+              From
+            </c-button>
+            </c-box>
+            <c-box width="50%" ml="2">
+            <c-button
+              bg="transparent"
+              borderRadius="15"
+              border="1px solid #9E9E9E"
+              size="md"
+              fontWeight="400"
+              fontSize="14px"
+              width="100%"
+              :_hover="{bg: 'transparent'}"
+            >
+               <i><CalenderIcon /></i>
+              To
+            </c-button>
+            </c-box>
+          </c-flex>
+          <c-box mt="4">
+            <c-text fontSize="md" fontWeight="bold">Amount</c-text>
+            <c-flex justify="space-between" mt="4">
+              <c-box>
+                <c-form-control pr="2">
+                  <c-form-label fontSize="sm">Minimum Amount</c-form-label>
+                  <c-input type="email" id="email" fontSize="sm" placeholder="Input minimum" borderRadius="15" />
+                </c-form-control>
+              </c-box>
+              <c-box>
+                <c-form-control pl="2">
+                  <c-form-label fontSize="sm">Maxmum Amount</c-form-label>
+                  <c-input type="email" id="email" fontSize="sm" placeholder="Imput maximum" borderRadius="15" />
+                </c-form-control>
+              </c-box>
+            </c-flex>
+          </c-box>
+          <c-flex pt="5" pb="8">
+            <c-button
+                size="md"
+                color="#FFFFFF"
+                border-radius="16px"
+                bg="#E27253;"
+                border="1px"
+                border-color="#E27253"
+                mt="4"
+                width="80%"
+                :_hover="{bg: '#E27253', opacity: .8}"
+                mr="2"
+              >
+                Segment
+              </c-button>
+              <c-button
+                size="md"
+                color="#FFFFFF"
+                border-radius="16px"
+                bg="#E27253;"
+                border="1px"
+                border-color="#E27253"
+                mt="4"
+                width="20%"
+                :_hover="{bg: '#E27253', opacity: .8}"
+              >
+                <c-icon name="close" size="12px" />
+              </c-button>
+          </c-flex>
+        </c-modal-body>
+      </c-modal-content>
+      <c-modal-overlay />
+    </c-modal>
   </c-box>
 </template>
 
 <script>
+import SettingIcon from '@/components/svg/SettingIcon'
+import CalenderIcon from '@/components/svg/CalenderIcon'
 export default {
+  components: {
+    SettingIcon,
+    CalenderIcon
+  },
   data () {
     return {
-      checkedItems: [false, false]
+      checkedItems: [false, false],
+      isOpen: false
     }
   },
   computed: {
@@ -78,6 +216,14 @@ export default {
     },
     isIndeterminate () {
       return this.checkedItems.some(Boolean) && !this.allChecked
+    }
+  },
+  methods: {
+    open () {
+      this.isOpen = true
+    },
+    close () {
+      this.isOpen = false
     }
   }
 }
